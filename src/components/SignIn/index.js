f7726f4a81f59e6464f2a8fc48f3fe2370279d25
melnee <Melanie.Neilan@gmail.com>
2020-12-12 23:05:17 -0800

    Added Login Functionality for Barebones Email and Password Users
    Added the ability to login for users who only registerd with email/password, and not google sign in.
    
    Extracted error messages from the auth object from firebase and placed them into the sign-in page state, then colored those messages red in the stylesheet.
    
    I don't like the default error messages however.
    
    To-Do:
    1. Make a custom error message if an email/password combo is not found in the system.
    2. Figure out how to check if an email is in the system in firebase in order to do this.

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,27 +1,102 @@
 import React, { Component } from 'react';
 import './styles.scss';
 import Button from './../forms/Button';
-import { signInWithGoogle } from './../../firebase/utils';
+import { signInWithGoogle, auth } from './../../firebase/utils';
 
+import FormInput from "./../forms/FormInput";
+import Buttonm from "./../forms/Button";
 
+const initialState = {
+    email: '',
+    password: '',
+    errors: []
+}
 
 
 class SignIn extends Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            ...initialState
+        };
+
+    this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(e) {
+        const { name, value} = e.target;
+        this.setState({
+            [name]: value
+        });
+    } 
 
     handleSubmit = async e =>{
         e.preventDefault();
+        const { email, password, errors } = this.state;
+
+        try{
+
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                ...initialState
+            });
+
+        } catch(err){
+            console.log(err);
+            const error = [err.message]
+            this.setState({
+                errors: error
+            });
+            return;
+        }
     }
 
     render(){
+        const { email, password, errors } = this.state;
         return(
             <div className = "signin">
                 <div className = "wrap">
                     <h2>
                         Login
                     </h2> 
-                    
+
+                    <div className = "errorWrap">
+                        {errors.length > 0 &&(
+                            <ul>
+                                {errors.map((err, index) => {
+                                    return (
+                                        <li key = {index}>
+                                            {err}
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        )}
+                    </div>
+
                     <div classnmae = "formWrap">
                         <form onSubmit = { this.handleSubmit }>
+
+                            <FormInput
+                                type = "email"
+                                name = "email"
+                                value = {email}
+                                placeholder = "Email"
+                                handleChange = {this.handleChange}
+                            ></FormInput>
+                            <FormInput
+                                type = "password"
+                                name = "password"
+                                value = {password}
+                                placeholder = "Password"
+                                handleChange = {this.handleChange}
+                            ></FormInput>
+
+                            <Button type = "submit">
+                                Login
+                            </Button>
+
                             <div className = "socialSignin">
                                 <div className = "row">
                                     <Button onClick={signInWithGoogle}>
